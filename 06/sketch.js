da4ec5f698e68e62d064538cf7d448f5b74aba70
Ayac98 <31419430+Ayac98@users.noreply.github.com>
2017-10-23 12:05:33 -0500

    Update sketch.js

--- 06/sketch.js
+++ 06/sketch.js
@@ -1,3 +1,4 @@
+/// Variaable y botones
 var boton1x = 8;
 var boton1y = 10;
 var boton1tam = 50;
@@ -17,7 +18,7 @@ var miPulpo;
 
 var filtro = 0;
 
-
+/// Función para cargar la imagen
 function preload(){
  miMar = loadImage("assets/Arrecife.jpg");
  miCaballo = loadImage("assets/Caballo.png");
@@ -35,38 +36,40 @@ function draw() {
   background(255);
   
   image(miMar,0,0);
-
+/// Boton de filtro azul
  fill(253,83,8);
 	rect(boton1x, boton1y, boton1tam, boton1tam);
-  
+ /// boton mascara de recorte  
 	fill(251,153,2);
 	rect(boton2x, boton2y, boton2tam, boton2tam);
-	
+/// boton de filtro sobreexpuestp	
   fill(254,254,51);
 	rect(boton3x, boton3y, boton3tam,boton3tam);
-  
+ /// boton de borrador
   fill(105,172,50);
 	rect(boton4x, boton4y, boton4tam, boton4tam);
-
+/// Botón filtro 1 ( tinta azul) 
 if (mouseIsPressed) {
     if (mouseX > boton1x  && mouseX < boton1x  + boton1tam &&
       mouseY > boton1y && mouseY < boton1y + boton1tam) {
       tint(25,84,154);
 
     }
- 
+ /// Botón filtro caballito de mar
  if (mouseX > boton2x && mouseX < boton2x + boton2tam &&
       mouseY > boton2y && mouseY < boton2y + boton2tam) {
       ///tint(210,98,7);
          miMar.mask(miCaballo);
     }
-
+/// Botón filtro sobre expuesto
   if (mouseX > boton3x && mouseX < boton3x + boton3tam &&
       mouseY > boton3y && mouseY < boton3y + boton3tam) {
-         miMar.mask(miPulpo);
-   
+         //miMar.mask(miPulpo);
+          image(miMar,0,0);
+      filter(POSTERIZE,3);
 
     }
+/// Botón borrador	
 if (mouseX > boton4x && mouseX < boton4x + boton4tam &&
       mouseY > boton4y && mouseY < boton4y + boton4tam) {
       miMar = loadImage("assets/Arrecife.jpg");
